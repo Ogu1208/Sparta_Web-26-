791f9f17cab8370195494d7bc1f32c38621f8784
Ogu1208 <kdasunb@naver.com>
2022-07-07 18:08:13 +0900

    flask get/post prac

---
+++ 스파르타코딩클럽/projects/prac/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+   return render_template('index.html')
+
+@app.route('/test', methods=['GET'])
+def test_get():
+   title_receive = request.args.get('title_give')   # title_give로 가져온 값을 가져와라
+   # title_receive = '봄날을 간다'
+   print(title_receive)
+   return jsonify({'result':'success', 'msg': '이 요청은 GET!!!!!!!'})
+
+@app.route('/test', methods=['POST'])
+def test_post():
+   title_receive = request.form['title_give']   # title_give로 가져온 값을 가져와라
+   print(title_receive)
+   return jsonify({'result':'success', 'msg': '이 요청은 POST!'})
+
+if __name__ == '__main__':
+   app.run('0.0.0.0',port=5000,debug=True)
